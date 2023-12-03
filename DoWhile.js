fb0a30f7752cfd0b157000d943d90518911f9d4d
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-12-03 17:23:52 +0100

    Archivos colocados en sus carpetas

--- DoWhile.js
+++
@@ -1,8 +0,0 @@
-const prompt=require('prompt-sync')();
-let num;
-
-do{
-    num=prompt('Introduce un numero positivo: ')
-}while(num < 0)
-
-console.log(`Bienvenido haz introducido el numero: ${num}`)
